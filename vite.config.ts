559cd549745c9e6cf7d41850c36e471421b4080e
XOOPO <apexnovaop008@gmail.com>
2025-11-26 21:02:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
-import { resolve } from 'path';
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
-const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname;
-
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/",   // ✅ TAMBAHKAN BARIS INI
+  base: "/",
+
   plugins: [react(), tailwindcss()],
-  build: {
-    sourcemap: true,
-  },
+
   resolve: {
     alias: {
-      '@': resolve(projectRoot, 'src')
+      "@": resolve(__dirname, "src")
     }
+  },
+
+  build: {
+    sourcemap: true,
   },
 });
